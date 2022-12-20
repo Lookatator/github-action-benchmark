1f86f96fcce501efeeb6df517f7c040a16bd411c
Luca Grillotti <luca.grillotti@gmail.com>
2022-12-20 16:31:23 +0100

    test customSmallerIsBetter

--- examples/pytest/bench.py
+++ examples/pytest/bench.py
@@ -1,8 +1,49 @@
+import json
+from pathlib import Path
+
 from fib import fib
 import pytest
+import time
 
-def test_fib_10(benchmark):
-    benchmark(fib, 10)
+NAME_FILE = "logs.json"
 
-def test_fib_20(benchmark):
-    benchmark(fib, 20)
+def test_fib_10():
+    if Path(NAME_FILE).exists():
+        with open(NAME_FILE, "r") as f:
+            data = json.load(f)
+    else:
+        data = []
+    start = time.time()
+    _ = fib(10)
+    end = time.time()
+    name_file = Path(__file__).name
+    data.append({
+        "name": f"{name_file}::{test_fib_10.__name__}",
+        "unit": "seconds",
+        "value": f"{end - start}",
+    })
+    with open(NAME_FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def test_fib_20():
+    if Path(NAME_FILE).exists():
+        with open(NAME_FILE,
+                  "r") as f:
+            data = json.load(f)
+    else:
+        data = []
+    start = time.time()
+    _ = fib(10)
+    end = time.time()
+    name_file = Path(__file__).name
+    data.append({
+        "name": f"{name_file}::{test_fib_20.__name__}",
+        "unit": "seconds",
+        "value": f"{end - start}",
+    })
+    with open(NAME_FILE,
+              "w") as f:
+        json.dump(data,
+                  f,
+                  indent=4)
